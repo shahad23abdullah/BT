ab5969b874b8a8c5ffa494f96a01cc149df4515d
shahad23abdullah <shahad127.a@gmail.com>
2024-02-26 02:40:16 +0300

    L3

--- BudgetTracker/apps/budgetmodule/views.py
+++ BudgetTracker/apps/budgetmodule/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def index(request):
     #study the request 
     return render(request ,'budgetmodule/index.html') # rendering a template 
+
+
+
+def budget(request):
+    return render(request ,'budgetmodule/userBudget.html')
