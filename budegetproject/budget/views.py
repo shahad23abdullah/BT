9679acdedb1595b5311f30ed4e95fd59f82fa11c
shahad23abdullah <shahad127.a@gmail.com>
2024-05-12 16:12:05 +0300

    Add existing project files to Git

---
+++ budegetproject/budget/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def project_list(request):
+    return render(request , 'budget/project_list.html')
+
+
+
+def project_detail(request , project_slug):
+    #correct project
+    return render(request , 'budget/project_detail.html')
