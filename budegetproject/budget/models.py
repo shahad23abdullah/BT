eb9d9da5ce0c04efb2d7b0eab367c1e6cea8b0b9
= <=>
2024-05-17 16:38:43 +0300

    megrate models

--- budegetproject/budget/models.py
+++ budegetproject/budget/models.py
@@ -3,21 +3,19 @@ from django.utils.text import slugify
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100 , unique= True , blank= True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
     budget = models.IntegerField()
-    
-    def save(self , *args , **kwargs):
-        self.slug = slugify(self.name)
-        super(Project , self).save(*args , **kwargs)
-        
-        
-class Category (models.Model):
-    project = models.ForeignKey(Project , on_delete= models.CASCADE)
+
+def save(self, *args, ** kwargs):
+    self.slug = slugify(self.name)
+    super(Project, self).save(*args, ** kwargs)
+
+class Category(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
 
 class Expense(models.Model):
-    project = models.ForeignKey(Project , on_delete= models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    amount = models.DecimalField(max_digits= 8 , decimal_places= 2)
-    category = models.ForeignKey(Category , on_delete= models.CASCADE )
-    
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
