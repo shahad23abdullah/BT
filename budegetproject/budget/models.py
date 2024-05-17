919d2c8f3f381a5229b73805c925b754e0e207de
= <=>
2024-05-17 16:09:32 +0300

    add models

--- budegetproject/budget/models.py
+++ budegetproject/budget/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class project(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100 , unique= True , blank= True)
+    budget = models.IntegerField()
+    
+    def save(self , *args , **kwargs):
+        self.slug = slugify(self.name)
+        super(Project , self)
+        
+    
