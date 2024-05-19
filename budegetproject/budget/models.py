d72538ceb4d3730513aa0e768ab8296a4363b62e
= <=>
2024-05-19 21:38:26 +0300

    styleing and last changes

--- budegetproject/budget/models.py
+++ budegetproject/budget/models.py
@@ -27,3 +27,7 @@ class Expense(models.Model):
     title = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    
+    class Meta:
+        ordering = ('-amount',)
